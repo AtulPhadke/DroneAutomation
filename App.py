c15cbabec96335e007ba9d2878a15fb69d6400a8
AtulPhadke <51861378+AtulPhadke@users.noreply.github.com>
2019-09-10 19:32:16 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from flask import render_template, request
+import time
+import os
+app = Flask(__name__)
+#! /usr/bin/python
 
+from gps import *
+import time
+import os
+
+@app.route('/')
+def function():
+    return render_template("index.html")
+
+@app.route('/power')
+def power():
+    text = request.args.get('Power')
+    return render_template("power.html", text=text)
