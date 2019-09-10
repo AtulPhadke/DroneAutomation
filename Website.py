0a9542b87aa652468648252d0ef983e95e98f26c
AtulPhadke <51861378+AtulPhadke@users.noreply.github.com>
2019-09-10 19:31:43 -0400

    Update Website.py

--- Website.py
+++ Website.py
@@ -1,1 +1,12 @@
-import time
+import os
+import random
+from gps import *
+from time import *
+
+x = random.randint(1000, 9999)
+from App import app
+if __name__ == "__main__":
+    app.debug = True
+    host = os.environ.get('IP', '192.168.1.14')
+    port = int(os.environ.get('PORT', x))
+    app.run(host=host, port=port)
